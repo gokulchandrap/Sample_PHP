f8a437aee884f096eefe454eaed8086a1b4d12e3
gokulchandrapr <gokulchandra048@gmail.com>
2018-03-20 04:20:57 +0000

    commit-red

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 echo "<br>Welcome to OpenShift!!";
-echo "<br>This is Deployment1";
-echo "<br>############### .    Deployment_Name: Blue .  ###################";
+echo "<br>This is Deployment2";
+echo "<br>############### .    Deployment_Name: Red .  ###################";
